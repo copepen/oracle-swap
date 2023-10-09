8e346dabc5a5e2c177a76b82d4f10d5c503550f1
daiki saito <daiki.saito92@gmail.com>
2023-10-09 10:09:53 +0900

    fix: update deploy script

--- contract/src/scripts/Deploy.sol
+++ contract/src/scripts/Deploy.sol
@@ -44,29 +44,40 @@ abstract contract Deploy is Script {
     }
 }
 
-contract DeployMumbai is Deploy {
+contract DeployTokens is Deploy {
+    function run() external {
+        // deploy Tokens
+        _deployMyToken("Base token", "BST");
+        _deployMyToken("Quote token", "QUT");
+    }
+}
+
+contract DeploySwap is Deploy {
     function run() external {
         address pyth = 0xff1a0f4744e8582DF1aE09D5611b887B6a12925C;
         bytes32 baseTokenPriceId = 0x08f781a893bc9340140c5f89c8a96f438bcfae4d1474cc0f688e3a52892c7318;
         bytes32 quoteTokenPriceId = 0x1fc18861232290221461220bd4e2acd1dcdfbc89c84092c93c18bdc7756c1588;
         address baseToken = 0x6114446F40b1470095C999865a1a8bF98C649c56;
         address quoteToken = 0x4dF5E254b96A9F4CaCB1eb5eE843931A1a9e191b;
+
+        // deploy OracleSwap contract
+        _deployOralceSwap(
+            pyth,
+            baseTokenPriceId,
+            quoteTokenPriceId,
+            baseToken,
+            quoteToken
+        );
+    }
+}
+
+contract DeploySwapPair is Deploy {
+    function run() external {
+        address baseToken = 0x6114446F40b1470095C999865a1a8bF98C649c56;
+        address quoteToken = 0x4dF5E254b96A9F4CaCB1eb5eE843931A1a9e191b;
         address oracleSwap = 0xF50e5b2b1037f9Ab7B4f4EfE3dBeD5f0CAa343Bc;
 
-        // 1. deploy Tokens
-        // _deployMyToken("Base token", "BST");
-        // _deployMyToken("Quote token", "QUT");
-
-        // 2. deploy OracleSwap contract
-        // _deployOralceSwap(
-        //     pyth,
-        //     baseTokenPriceId,
-        //     quoteTokenPriceId,
-        //     baseToken,
-        //     quoteToken
-        // );
-
-        // 3. deploy OracleSwapPair contract
+        // deploy OracleSwapPair contract
         _deployOralceSwapPair(baseToken, quoteToken, oracleSwap);
     }
 }
